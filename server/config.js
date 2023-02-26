cfe01e2e917567aec3c14447a1537a8e303acf18
Mumtaz Che Ismail <aremanzh@gmail.com>
2023-02-26 23:10:59 +0800

    Delete config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-
-// const DATABASE = "mongodb+srv://dbuser:dbpassword@your_db_name.pbn7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-export const DATABASE = "mongodb+srv://react-task:Us4UHUb8c5nGbOU2@cluster0.wjiddo6.mongodb.net/task?retryWrites=true&w=majority";
-// const DATABASE = "mongodb://localhost:27017/task";
-const JWT_SECRET = "3jIF26c38R0Dg4N5W2y7";
-const SENDGRID_KEY = "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";
-const EMAIL_FROM = "test@mumtaaz.xyz";
